36d2ccfddbd6c9ec8775cf8313a6a1d2fffd6d65
Jonas Bernoulli <jonas@bernoul.li>
2013-09-19 21:14:41 +0200

    initial import

---
+++ dim-autoload.el
@@ -0,0 +1,89 @@
+;;; dim-autoload.el --- dim complete autoload cookie lines
+
+;; Copyright (C) 2013  Jonas Bernoulli
+
+;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Version: 1.0.0
+;; Homepage: http://github.com/tarsius/dim-autoload
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Dim autoload cookies.
+
+;; Unlike the built-in font-lock keyword which only changes the
+;; appearance of the "autoload" substring of the autoload cookie
+;; line and repurposes the warning face also used elsewhere, the
+;; keyword added here changes the appearance of the complete line
+;; using a dedicated face.
+
+;; That face is intended for dimming.  While you are making sure
+;; your library contains all the required autoload cookies you can
+;; customize the face to temporarily make the cookies more visible.
+
+;; To install the keywords add this to your init file:
+;;
+;;    (global-dim-autoload-cookies-mode 1)
+
+;; You might also want to dim the cookie lines even more by using
+;; a foreground color in `dim-autoload-cookies-line' that is very
+;; close to the `default' background color.
+
+;;; Code:
+
+(defface dim-autoload-cookie-line
+  '((t (:inherit shadow)))
+  "Face for autoload cookie lines.
+
+This face is only used when the appropriate font-lock keyword
+for autoload cookie lines has been installed.  To do so enable
+`global-dim-autoload-cookies-mode'."
+  :group 'faces)
+
+(defconst dim-autoload-add-font-lock-keywords
+  '(("^;;;###[-a-z]*autoload.*$" 0 'dim-autoload-cookie-line t)))
+
+(defvar dim-autoload-cookies-mode-lighter "")
+
+;;;###autoload
+(define-minor-mode dim-autoload-cookies-mode
+  "Toggle dimming autoload cookie lines.
+You likely want to enable this globally
+using `global-dim-autoload-cookies-mode'."
+  :lighter dim-autoload-cookies-mode-lighter
+  (if dim-autoload-cookies-mode
+      (font-lock-add-keywords  nil dim-autoload-add-font-lock-keywords 'end)
+    (font-lock-remove-keywords nil dim-autoload-add-font-lock-keywords)))
+
+;;;###autoload
+(define-globalized-minor-mode global-dim-autoload-cookies-mode
+  dim-autoload-cookies-mode turn-on-dim-autoload-cookies-mode-if-desired)
+
+;;;###autoload
+(defun turn-on-dim-autoload-cookies-mode-if-desired ()
+  (when (derived-mode-p 'emacs-lisp-mode)
+    (dim-autoload-cookies-mode 1)))
+
+;; Ps: This is a lot of boilerplate; the same could be
+;;     achieved using half a dozen lines in your init file.
+
+(provide 'dim-autoload)
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+;;; dim-autoload.el ends here
