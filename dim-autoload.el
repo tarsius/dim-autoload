54bd356cdac7bdae2bdff994b048f847374cec6c
Jonas Bernoulli <jonas@bernoul.li>
2024-04-15 17:34:55 +0200

    Update contact information

--- dim-autoload.el
+++ dim-autoload.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2013-2024 Jonas Bernoulli
 
-;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Author: Jonas Bernoulli <emacs.dim-autoload@jonas.bernoulli.dev>
 ;; Homepage: https://github.com/tarsius/dim-autoload
 ;; Keywords: convenience
 
