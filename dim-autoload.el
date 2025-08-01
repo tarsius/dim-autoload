31ba7cf40f5da6b6157c71917599cf718fd68936
Jonas Bernoulli <jonas@bernoul.li>
2025-08-01 11:45:56 +0200

    Release version 2.1.0

--- dim-autoload.el
+++ dim-autoload.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/dim-autoload
 ;; Keywords: convenience
 
-;; Package-Version: 2.0.8
+;; Package-Version: 2.1.0
 ;; Package-Requires: ((emacs "26.1") (compat "30.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
